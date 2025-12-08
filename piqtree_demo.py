a8dca2fccfb3c4cec046116dbe3fe3a5cdf44060
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-12-09 10:11:01 +1100

    MAINT: update code for new analysis
    [CHANGED] we use the frequency optin FO, since then the
        nucleotide frequencies are numerically optimised rather
        than taken as the average across the observed sequences.
        This should improve the performance of root placement.
    
    [CHANGED] Excluding alignments whose length is less than 450.
        This is pretty arbitrary, but was chosen because it was
        divisible by three (important as we have protein coding
        sequence). I've filtered by length because short alignments
        will add noise in the analysis.

--- piqtree_demo.py
+++ piqtree_demo.py
@@ -8,10 +8,15 @@ from tqdm import tqdm
 aln_dir = Path("turtle_partitions/")
 
 trees = []
+omitted = 0
 for aln_file in tqdm(list(aln_dir.glob("*.fa"))):
     aln = load_aligned_seqs(aln_file, moltype="dna")
-    trees.append(build_tree(aln, "UNREST"))
+    if len(aln) < 450:
+        omitted += 1
+        continue
+    trees.append(build_tree(aln, "UNREST+FO"))
 
 supertree = construct_supertree(trees, pcg_weighting="branch")
 
 print(supertree)
+print(f"Omitted {omitted} partitions due to length < 450")
